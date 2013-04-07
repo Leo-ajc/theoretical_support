edb469004dd8b883919d3f924f282060897154cf
Leo-ajc <leo.campbell@gmail.com>
2013-04-07 14:22:39 +0100

    change for production enviroment for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SupportFu::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # for Heroku hosting.
 
   # Generate digests for assets URLs
   config.assets.digest = true
